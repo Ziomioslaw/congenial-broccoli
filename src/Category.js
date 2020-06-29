78f460fab5b25159d3fdbf9fc937af0e4a9e7da6
Ziomioslaw <ziomioslaw@wp.pl>
2020-06-29 20:48:31 +0200

    Improve the Header

--- src/Category.js
+++ src/Category.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import Card from 'react-bulma-components/lib/components/card';
 import Content from 'react-bulma-components/lib/components/content';
+import Button from 'react-bulma-components/lib/components/button';
+import Section from 'react-bulma-components/lib/components/section';
+
 
 export function Category(category) {
-  return (<Card key={category.id}>
+  return (<Section key={category.id}>
+    <Card>
       <Card.Header>
         <Card.Header.Title>{category.name}</Card.Header.Title>
       </Card.Header>
       <Content>
         {category.directory}
+        <Button>Delete</Button>
       </Content>
-      <Card.Footer>
-        <Card.Footer.Item></Card.Footer.Item>
-      </Card.Footer>
-    </Card>);
+    </Card>
+    </Section>);
 }
--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import Box from 'react-bulma-components/lib/components/box';
 import Heading from 'react-bulma-components/lib/components/heading';
+import Hero from 'react-bulma-components/lib/components/hero';
 
 export function Header() {
-  return (<Box>
-    <Heading>
-      Download
-    </Heading>
-  </Box>);
+  return (
+    <Hero color="primary" >
+      <Hero.Body>
+        <Heading>Download</Heading>
+        <Heading subtitle size={5}>
+          <a href="http://gimpuj.info/download">Check the page</a>
+        </Heading>
+      </Hero.Body>
+    </Hero>
+  );
 }
