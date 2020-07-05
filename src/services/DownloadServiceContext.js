e625f1031f71a834cdf38523c4d79b4a9ca392ae
Ziomioslaw <ziomioslaw@wp.pl>
2020-07-05 19:41:53 +0200

    Add editorconfig

--- src/services/DownloadServiceContext.js
+++ src/services/DownloadServiceContext.js
@@ -2,17 +2,17 @@ import React from "react";
 
 class DownloadService {
 
-    static REST_URL = 'https://my-json-server.typicode.com/ziomioslaw/congenial-broccoli/';
+  static REST_URL = 'https://my-json-server.typicode.com/ziomioslaw/congenial-broccoli/';
 
-    async getCategories() {
-        return fetch(`${DownloadService.REST_URL}categories`)
-            .then(res => res.json())
-    }
+  async getCategories() {
+    return fetch(`${DownloadService.REST_URL}categories`)
+      .then(res => res.json());
+  }
 
-    async getCategory(categoryId) {
-        return fetch(`${DownloadService.REST_URL}categories/${categoryId}`)
-            .then(res => res.json());
-    }
+  async getCategory(categoryId) {
+    return fetch(`${DownloadService.REST_URL}categories/${categoryId}`)
+      .then(res => res.json());
+  }
 }
 
 const DownloadServiceContext = React.createContext(new DownloadService());
