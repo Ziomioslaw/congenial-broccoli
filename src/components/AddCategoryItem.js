3c3b2762b969cfc55126be2ee4c6e9d8310d9c10
Ziomioslaw <ziomioslaw@wp.pl>
2020-08-01 15:34:16 +0200

    Control set up form

--- src/components/AddCategoryItem.js
+++ src/components/AddCategoryItem.js
@@ -1,14 +1,32 @@
 import React from 'react';
-import { Field, Control, Label, InputFile } from 'react-bulma-components/lib/components/form';
+import Columns from 'react-bulma-components/lib/components/columns';
+import { Field, Control, Label, InputFile, Input } from 'react-bulma-components/lib/components/form';
 import Icon from 'react-bulma-components/lib/components/icon';
+import Button from 'react-bulma-components/lib/components/button';
 
 export default function AddCategoryItem() {
 
-  return (
-    <Field>
-      <Label>File</Label>
-      <Control>
-        <InputFile icon={<Icon icon="upload" />} />
-      </Control>
-    </Field>)
+  return (<Columns>
+
+      <Columns.Column>
+        <Field>
+          <Label>File</Label>
+          <Control>
+            <InputFile icon={<Icon icon="upload" />} />
+            <Button type="primary">Submit</Button>
+          </Control>
+        </Field>
+      </Columns.Column>
+
+      <Columns.Column>
+        <Field>
+          <Label>Wget link</Label>
+          <Control>
+            <Input placeholder="Text input" />
+            <Button type="primary">Submit</Button>
+          </Control>
+        </Field>
+      </Columns.Column>
+
+    </Columns>)
 }
